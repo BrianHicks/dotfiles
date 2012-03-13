d9c03d7bd8b7853f7b8a41c35115dab22c8beaad
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-03-13 13:36:14 -0500

    update update script

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,8 +1,4 @@
-for submodule in $(git submodule | cut -d" " -f3); do
-    pushd $submodule
-        git stash
-        git pull
-        git checkout master
-        git stash pop
-    popd
-done
+git submodule init
+git submodule update
+git submodule foreach git checkout master
+git submodule foreach git pull
