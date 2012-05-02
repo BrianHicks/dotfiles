7ade5f87d1e24e4aea2640feffa1f656c8d533f4
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-05-02 12:02:38 -0500

    update-submodules fixes vim-powerline at 99277d

--- update-submodules.sh
+++ update-submodules.sh
@@ -2,3 +2,8 @@ git submodule init
 git submodule update
 git submodule foreach git checkout master
 git submodule foreach git pull
+
+# update vim-powerline to the last working commit
+pushd dotfiles/.vim/bundle/vim-powerline
+    git checkout 99277d9
+popd
