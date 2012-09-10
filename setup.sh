ac50e656f1d9606a9d2df7180e97619ffbf916b7
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-09-10 17:10:10 -0500

    modify scripts for vundle

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
+./update-bundle.sh
 for f in $(ls -A dotfiles/); do ln -s `pwd`/dotfiles/$f ~; done
--- update-submodules.sh
+++
@@ -1,9 +0,0 @@
-git submodule init
-git submodule update
-git submodule foreach git checkout master
-git submodule foreach git pull
-
-# update vim-powerline to the last working commit
-pushd dotfiles/.vim/bundle/vim-powerline
-    git checkout 99277d9
-popd
