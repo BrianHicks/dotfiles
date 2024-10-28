ed4d1a63e0d9e0ea21edfc1eee93a798c09d6e75
Brian Hicks <brian@brianthicks.com>
2024-10-28 13:57:31 -0500

    refine jq query

--- dotfiles/Library/Application Support/xbar/plugins/tw.1m.sh
+++ dotfiles/Library/Application Support/xbar/plugins/tw.1m.sh
@@ -3,4 +3,6 @@ set -euo pipefail
 
 PATH="$PATH:/opt/homebrew/bin"
 
-task export | jq -r 'map(select(.status == "pending" and .rtype == null and .depends == null)) | sort_by(.urgency) | reverse | "\(.[0].id): \(if .[0].description | length > 30 then .[0].description | .[0:30] + "…" else .[0].description end)\n---\n\(.[0:30] | map("\(.id): \(.description)\n-- Mark Done | shell=task | param1=\(.uuid) | param2=done | refresh=true\n--project: \(.project)\n-- priority: \(.priority)\n-- uregency: \(.urgency)") | join("\n"))"'
+NOW="$(date -u "+%Y%m%dT%H%M%SZ")"
+
+task export | jq --arg NOW "$NOW" -r 'map(select(.status == "pending" and .depends == null and (.wait == null or .wait <= $NOW))) | sort_by(.urgency) | reverse | "\(.[0].id): \(if .[0].description | length > 30 then .[0].description | .[0:30] + "…" else .[0].description end)\n---\n\(.[0:30] | map("\(.id): \(.description)\n-- Mark Done | shell=/opt/homebrew/bin/task | param1=\(.uuid) | param2=done | refresh=true\n--project: \(.project)\n-- priority: \(.priority)\n-- urgency: \(.urgency)\(if .recur then "\n-- recur: \(.recur)" else "" end)\n-- wait: \(.wait)\n-- due: \(.due)") | join("\n"))"'
